df0685ef2c8a63c1bb56db71c2d4378051338b32
PSEH-13 <52617507+PSEH-13@users.noreply.github.com>
2020-02-09 15:24:51 +0530

    Update LeagueService.java

--- football/src/main/java/com/assessment/football/service/LeagueService.java
+++ football/src/main/java/com/assessment/football/service/LeagueService.java
@@ -2,11 +2,11 @@ package com.assessment.football.service;
 
 import java.util.Map;
 
-import com.assessment.football.exception.VaildationException;
+import com.assessment.football.exception.CustomException;
 import com.assessment.football.vo.LeagueRequestVO;
 
 public interface LeagueService {
 
-	Map<String, String> getStatus(LeagueRequestVO leagueRequest) throws VaildationException;
+	Map<String, String> getStatus(LeagueRequestVO leagueRequest) throws CustomException;
 
 }
