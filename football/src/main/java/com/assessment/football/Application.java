80ebf537e19baf0f7c3c16058b51c4181530c4b1
PSEH-13 <52617507+PSEH-13@users.noreply.github.com>
2020-02-09 14:29:06 +0530

    Update Application.java

--- football/src/main/java/com/assessment/football/Application.java
+++ football/src/main/java/com/assessment/football/Application.java
@@ -3,7 +3,9 @@ package com.assessment.football;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan
@@ -12,6 +14,12 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+    public RestTemplate restTemplate() {
+    	RestTemplate rt = new RestTemplate();
+    	return rt;
     }
     
 }
